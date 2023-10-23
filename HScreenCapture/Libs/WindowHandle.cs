fe0306bf8c6da22300297e8c7212becc24c4cefb
Herbert Agosto <herbertagosto@gmail.com>
2023-10-23 13:38:45 +0800

    add libs > WindowHandle.cs

---
+++ HScreenCapture/Libs/WindowHandle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HScreenCapture.Libs
+{
+    class WindowHandle
+    {
+        // find window with class name and main title
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string windowClassName, string windowMainTitle);
+        public static IntPtr GetHandle(string windowClassName, string windowMainTitle)
+        {
+            return FindWindow(windowClassName, windowMainTitle);
+        }
+
+        // set window in front
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+    }
+}
