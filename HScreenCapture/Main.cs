b492a59e6d00c99d4cc3e0f1bb2a3c5aacf188e5
Herbert Agosto <herbertagosto@gmail.com>
2023-10-23 13:47:36 +0800

    add main methods

--- HScreenCapture/Main.cs
+++ HScreenCapture/Main.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HScreenCapture.Libs;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
 
 namespace HScreenCapture
 {
     public class Main
     {
+        /// <summary>
+        /// Screen capture the screen
+        /// </summary>
+        /// <returns></returns>
+        public Image GetImage()
+        {
+            return new ScreenCapture().CaptureScreen();
+        }
+        /// <summary>
+        /// Screen capture a window
+        /// </summary>
+        /// <param name="windowClassName">Use the Microsoft Spy++ app to get the window class name</param>
+        /// <param name="windowMainTitle">Use the Microsoft Spy++ app to get the window class name</param>
+        /// <returns></returns>
+        public Image GetImage(string windowClassName, string windowMainTitle)
+        {
+            var windowHandle = WindowHandle.GetHandle(windowClassName, windowMainTitle);
+            return new ScreenCapture().CaptureWindow(windowHandle);
+        }
+        /// <summary>
+        /// Save image to clipboard
+        /// </summary>
+        /// <param name="image"></param>
+        public void SaveToClipboard(Image image)
+        {
+            Clipboard.SetImage(image);
+        }
+        /// <summary>
+        /// Save image to PNG file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="image"></param>
+        public void SaveToPNG(string path, Image image)
+        {
+            SaveToFile(path, image, ImageFormat.Png);
+        }
+        /// <summary>
+        /// Save image to file
+        /// Must add reference to System.Drawing.dll
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="image"></param>
+        /// <param name="imageFormat"></param>
+        public void SaveToFile(string path, Image image, ImageFormat imageFormat)
+        {
+            ScreenCapture sc = new ScreenCapture();
+            image.Save(path, imageFormat);
+        }
     }
 }
